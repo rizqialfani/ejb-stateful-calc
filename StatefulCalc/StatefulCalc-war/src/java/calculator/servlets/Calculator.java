730893227e56e1977a6399cfb4c196a5a72e34c3
rizqialfani01 <55873026+rizqialfani01@users.noreply.github.com>
2020-06-09 00:11:52 +0700

    phase 2 work

--- StatefulCalc/StatefulCalc-war/src/java/calculator/servlets/Calculator.java
+++ StatefulCalc/StatefulCalc-war/src/java/calculator/servlets/Calculator.java
@@ -137,23 +137,18 @@ public class Calculator extends HttpServlet {
                 out.println("  <meta http-equiv='X-UA-Compatible' content='IE=edge'>");
                 out.println("  <meta name='viewport' content='width=device-width, initial-scale=1'>");
 
-                out.println("  <!-- Estilos -->");
-                out.println("  <link href='css/bootstrap.css' rel='stylesheet'>");
+                out.println("  <link href='http://begoursus.github.io/calculadora-bt/site/css/bootstrap.css' rel='stylesheet'>");
+                out.println("  <link rel='stylesheet' type='text/css' href='http://begoursus.github.io/calculadora-bt/site/css/supernice.css'>");
+                out.println("  <link rel='stylesheet' type='text/css' href='http://begoursus.github.io/calculadora-bt/site/css/styles.css'>");
+                out.println("  <link rel='stylesheet' type='text/css' href='http://begoursus.github.io/calculadora-bt/site/css/styles_calculadora.css'>");
 
-                out.println("  <link rel='stylesheet' type='text/css' href='css/supernice.css'>");
-
-                out.println("  <link rel='stylesheet' type='text/css' href='css/styles.css'>");
-                out.println("  <link rel='stylesheet' type='text/css' href='css/styles_calculadora.css'>");
-
-                out.println("<!-- Fuentes -->");
                 out.println("  <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>");
                 out.println("  <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700,700italic' rel='stylesheet' type='text/css'>");
                 out.println("  <link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono' rel='stylesheet' type='text/css'>");
                 out.println("  <link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet' type='text/css'>");
                 out.println("  <link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>");
-
-
                 out.println("</head>");
+                
                 out.println("<body style='background-color: #99BADD;'>");
                 out.println("  <header class='container' style='width: 50%;'>");
                 out.println("    <nav class='navbar navbar-inverse' style='background-color: #4E5F72; border-color: #4E5F72;'>");
@@ -169,66 +164,49 @@ public class Calculator extends HttpServlet {
                 out.println("        <!-- grupo LCD + Botón atrás -->");
                 out.println("        <div class='col-xs-12 col-sm-12' id='resultado'>");
                 out.println("          <div class='input-group  has-success' style='width: 100%;'>");
-                out.println("            <input type='text' class='form-control' value='' id='lcd' style='border-radius: 4px;'>");
+                out.println("            <form name=\"calculator\">");
+                out.println("            <input type=\'text\' class=\'form-control\' value=\'\' id=\'lcd\' style=\'border-radius: 4px;\' name=\'answer\' />");
                 out.println("          </div>"); 
-                out.println("          <div id='botones_lcd' class='pull-left hidden-xs'>");
-                out.println("            <div>");
-                out.println("              <button type='button' class='btn btn-success btn-sm op_unitario btn_lcd' id='b_atras' title='Borra un caracter'>");
-                out.println("                <span class='glyphicon glyphicon-arrow-left' aria-hidden='true'></span>");
-                out.println("              </button>");
-                out.println("            </div>");
-                out.println("          </div>");
                 out.println("        </div>"); 
-                out.println("        <!-- Resultado -->");
 
-                out.println("      </div> <!-- Cabecera -->");
+                out.println("      </div>");
 
                 out.println("      <div class='container-fluid teclado '>");
                 out.println("          <div class='col-xs-12 col-sm-12 teclado-ordenador'>");
                 out.println("            <div class='row' id='tp-r1'>");
                 out.println("              <!-- <button class='btn btn-info bnt-md' data-toggle='modal' data-target='#myModal'> -->");
-                out.println("              <button class='btn btn-danger bnt-md op_borrar' id='b_borrar' title='Resetea la calculadora (elimina el valor actual, la memoria y el acumulador)'>C</button>");
-                out.println("              <button class='btn btn-danger bnt-md op_borrar' id='b_borrar_parcial' title='Resetea únicamente el valor actual'>CE</button>");
-                out.println("              <button class='btn btn-primary bnt-md op_binario' id='b_divide' title='División'>/</button>");
-                out.println("              <button class='btn btn-primary bnt-md op_binario' id='b_multiplica' title='Multiplicación'>*</button>");
+                out.println("              <button class='btn btn-danger bnt-md op_borrar' id='b_borrar_parcial' type='reset' value='reset' title='Reset'>CE</button>");
+                out.println("              <input type=\'button\' class=\'btn btn-primary bnt-md op_binario\' id=\'b_multiplica\' title=\'Mul\' style=\'width:31.5%;\' value=\" x \" onclick=\"calculator.answer.value += '*'\" />");
                 out.println("            </div>");
 
                 out.println("            <div class='row' id='tp-r2'>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num7'>7</button>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num8'>8</button>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num9'>9</button>");
-                out.println("              <button class='btn btn-primary bnt-md op_binario' id='b_suma' title='Suma'>+</button>");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num7\' type=\"button\" value=\" 7 \" onclick=\"calculator.answer.value += '7'\" />");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num8\' type=\"button\" value=\" 8 \" onclick=\"calculator.answer.value += '8'\" />");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num9\' type=\"button\" value=\" 9 \" onclick=\"calculator.answer.value += '9'\" />");
+                out.println("              <input type=\'button\' class=\'btn btn-primary bnt-md op_binario\' id=\'b_suma\' title=\'Sum\' value=\" + \" onclick=\"calculator.answer.value += '+'\" />");
                 out.println("            </div>");
                 out.println("            <div class='row' id='tp-r3'>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num4'>4</button>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num5'>5</button>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num6'>6</button>");
-                out.println("              <button class='btn btn-primary bnt-md op_binario' id='b_resta' title='Resta'>-</button>");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num4\' type=\"button\" value=\" 4 \" onclick=\"calculator.answer.value += '4'\" />");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num5\' type=\"button\" value=\" 5 \" onclick=\"calculator.answer.value += '5'\" />");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num6\' type=\"button\" value=\" 6 \" onclick=\"calculator.answer.value += '6'\" />");
+                out.println("              <input type=\'button\' class=\'btn btn-primary bnt-md op_binario\' id=\'b_resta\' title=\'Sub\' value=\" - \" onclick=\"calculator.answer.value += '-'\" />");
                 out.println("            </div>");
                 out.println("            <div class='row' id='tp-r4-r5'>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num1'>1</button>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num2'>2</button>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num3'>3</button>");
-                out.println("              <button class='btn btn-default bnt-md op_unitario' id='b_signo' title='Cambiar el signo'>&plusmn;</button>");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num1\' type=\"button\" value=\" 1 \" onclick=\"calculator.answer.value += '1'\" />");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num2\' type=\"button\" value=\" 2 \" onclick=\"calculator.answer.value += '2'\" />");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num3\' type=\"button\" value=\" 3 \" onclick=\"calculator.answer.value += '3'\" />");
+                out.println("              <input type=\'button\' class=\'btn btn-primary bnt-md op_binario\' id=\'b_divide\' title=\'Div\' value=\" / \" onclick=\"calculator.answer.value += '/'\" />");
                 out.println("            </div>");
                 out.println("            <div class='row'>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='punto' title='Separador decimal'>.</button>");
-                out.println("              <button class='btn btn-default bnt-md btn_numero' id='num0'>0</button>");
-                out.println("              <button class='btn btn-success bnt-md op_calcular' id='b_calcula' title='Resultado' style='width: 240%'>&equals;</button>");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'punto\' type=\"button\" title=\'Comma\' value=\" . \" onclick=\"calculator.answer.value += '.'\" />");
+                out.println("              <input class=\'btn btn-default bnt-md btn_numero\' id=\'num0\' type=\"button\" value=\" 0 \" onclick=\"calculator.answer.value += '0'\" />");
+                out.println("              <input class=\'btn btn-success bnt-md op_calcular\' id=\'b_calcula\' title=\'Result\' style=\'width: 224%\' type=\"button\" value=\" = \" onclick=\"calculator.answer.value = eval(calculator.answer.value)\" />");
                 out.println("            </div>");
-                out.println("          </div> <!--teclado numérico principal -->");
+                out.println("          </div>");
 
-                out.println("      </div> <!--calculadora -->");
+                out.println("      </div>");
                 out.println("    </div>");
 
-                out.println("    <div class='container calculadora'>"); 
-                out.println("      <div class='container-fluid cabecera' id='cabecera'  style='height: 97%;'>");
-                out.println("        <!-- grupo LCD + Botón atrás -->");
-                out.println("        <div class='col-xs-12 col-sm-12' id='resultado'>");
-                out.println("        </div>"); 
-                out.println("        <!-- Resultado -->");
-                out.println("      </div> <!-- Cabecera -->");
-                out.println("    </div>");
                 out.println("  </div>");
                 out.println("</body>");
                 out.println("</html>");
--- StatefulCalc/StatefulCalc-war/web/index.jsp
+++ StatefulCalc/StatefulCalc-war/web/index.jsp
@@ -7,12 +7,50 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Calculator Landing Page</title>
-    </head> 
-    <body>
-        <h2>Calculator</h2>
-        <a href="Calculator">Click here to use the Calculator</a>
-    </body>
+<head>
+  <title>Stateful Calculator</title>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Estilos -->
+  <link href="http://begoursus.github.io/calculadora-bt/site/css/bootstrap.css" rel="stylesheet">
+
+  <link rel="stylesheet" type="text/css" href="http://begoursus.github.io/calculadora-bt/site/css/supernice.css">
+
+  <link rel="stylesheet" type="text/css" href="http://begoursus.github.io/calculadora-bt/site/css/styles.css">
+  <link rel="stylesheet" type="text/css" href="http://begoursus.github.io/calculadora-bt/site/css/styles_calculadora.css">
+
+<!-- Fuentes -->
+  <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+
+
+</head>
+<body style="background-color: #99BADD;">
+  <header class="container" style="width: 50%;">
+    <nav class="navbar navbar-inverse" style="background-color: #4E5F72; border-color: #4E5F72;">
+      <div class="container-fluid">
+        <h1 style="text-align: center; color: white;">Calculator using Stateful Session Beans</h1>
+        <h3 style="text-align: center; color: white; margin: 10px 0 20px 0">M. Rizqi Alfani - 21120117140007</h3>
+      </div>
+    </nav>
+  </header>
+  <div style="display: flex; margin: 0 auto; width: 50%;">
+    <div class="container calculadora"> 
+      <div class="container-fluid teclado ">
+          <div class="col-xs-12 col-sm-12 teclado-ordenador">
+            <div class="row">
+              <a class="btn btn-success bnt-md op_calcular" id="b_calcula" title="Resultado" href="Calculator">Mulai Menghitung</a>
+            </div>
+          </div> <!--teclado numérico principal -->
+
+      </div> <!--calculadora -->
+    </div>
+  </div>
+</body>
 </html>
